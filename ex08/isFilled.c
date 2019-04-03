e8e715d5ccf867a8da2efff6b6a2a939cb8d116c
honoyr <honoyr@honoyrs-iMac.local>
2019-04-02 21:51:38 -0700

    ex04-ex18 done

--- ex08/isFilled.c
+++ ex08/isFilled.c
@@ -18,63 +18,7 @@
 
 #include "header.h"
 
-// int		lenn(int tmp, int base)
-// {
-// 	int		i;
-
-// 	i = 0;
-// 	if (tmp <= 0)
-// 		i++;
-// 	while (tmp != 0)
-// 	{
-// 		tmp /= base;
-// 		i++;
-// 	}
-// 	return (i);
-// }
-
-// void	ft_help(char *s, int val, int base, int len)
-// {
-// 	char	*c;
-
-// 	c = "0123456789ABCDEF";
-// 	if (val < 0 && base == 10)
-// 	{
-// 		if (val == -2147483648)
-// 		{
-// 			s[--len] = '8';
-// 			val = 214748364;
-// 		}
-// 		else
-// 			val *= -1;
-// 		s[0] = '-';
-// 	}
-// 	if (val == 0)
-// 		s[--len] = '0';
-// 	while (val)
-// 	{
-// 		s[--len] = c[val % base];
-// 		val /= base;
-// 	}
-// }
-
-// char	*ft_itoa_base(int val, int base)
-// {
-// 	int		len;
-// 	char	*str;
-
-// 	len = lenn(val, base);
-// 	if (!(str = (char*)malloc(sizeof(char) * len + 1)))
-// 		return (NULL);
-// 	str[len] = '\0';
-// 	ft_help(str, val, base, len);
-// 	return (str);
-// }
-
-int getPlace(unsigned int parkingRow, int pos)
+int isFilled(unsigned int parkingRow)
 {
-	// printf("ORIG =     %s\n", ft_itoa_base(parkingRow, 2));
-	// printf("SHIFT =    %s\n", ft_itoa_base(((parkingRow >> pos)), 2));
-	// printf("AND =  	   %s\n", ft_itoa_base(((parkingRow >> pos) & 10), 2));
-	return (parkingRow >> pos) & 1;
+	return parkingRow && (parkingRow & (-(~parkingRow))) == 0;	
 }
--- ex15/longestSequence.c
+++ ex15/longestSequence.c
@@ -15,66 +15,14 @@
 #include <unistd.h> //fork, write, sleep...
 #include <stdlib.h> //malloc, free, exit...
 #include <limits.h> //malloc, free, exit...
+#include <math.h>
 
 #include "header.h"
 
-// int		lenn(int tmp, int base)
-// {
-// 	int		i;
-
-// 	i = 0;
-// 	if (tmp <= 0)
-// 		i++;
-// 	while (tmp != 0)
-// 	{
-// 		tmp /= base;
-// 		i++;
-// 	}
-// 	return (i);
-// }
-
-// void	ft_help(char *s, int val, int base, int len)
-// {
-// 	char	*c;
-
-// 	c = "0123456789ABCDEF";
-// 	if (val < 0 && base == 10)
-// 	{
-// 		if (val == -2147483648)
-// 		{
-// 			s[--len] = '8';
-// 			val = 214748364;
-// 		}
-// 		else
-// 			val *= -1;
-// 		s[0] = '-';
-// 	}
-// 	if (val == 0)
-// 		s[--len] = '0';
-// 	while (val)
-// 	{
-// 		s[--len] = c[val % base];
-// 		val /= base;
-// 	}
-// }
-
-// char	*ft_itoa_base(int val, int base)
-// {
-// 	int		len;
-// 	char	*str;
-
-// 	len = lenn(val, base);
-// 	if (!(str = (char*)malloc(sizeof(char) * len + 1)))
-// 		return (NULL);
-// 	str[len] = '\0';
-// 	ft_help(str, val, base, len);
-// 	return (str);
-// }
-
-int getPlace(unsigned int parkingRow, int pos)
+int longestSequence(unsigned int parkingRow)
 {
-	// printf("ORIG =     %s\n", ft_itoa_base(parkingRow, 2));
-	// printf("SHIFT =    %s\n", ft_itoa_base(((parkingRow >> pos)), 2));
-	// printf("AND =  	   %s\n", ft_itoa_base(((parkingRow >> pos) & 10), 2));
-	return (parkingRow >> pos) & 1;
+	int i;
+	for (i = 0 ; parkingRow; i++)
+		parkingRow &= (parkingRow << 1);
+	return i;
 }
